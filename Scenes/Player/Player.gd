9c01b263c4b4619797741a285d842c722f947f23
An Truong <truongtaman2004@gmail.com>
2026-05-11 20:18:32 +0700

    Init BTL Game

---
+++ Scenes/Player/Player.gd
@@ -0,0 +1,12 @@
+extends CharacterBody3D
+
+# ALl player script goes here
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass

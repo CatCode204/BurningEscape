39c9b552f9cf89b5c75a5e79a488d346e131773e
An Truong <truongtaman2004@gmail.com>
2026-05-11 20:38:16 +0700

    Some setting to map and player

--- Scenes/Player/Player.gd
+++ Scenes/Player/Player.gd
@@ -1,12 +1,15 @@
 extends CharacterBody3D
 
 # ALl player script goes here
+@export var _move_speed : float = 5.0;
+@export var _sprint_speed : float = 8.0;
+@export var _accelerate_speed : float = 0.2;
+@export var _deccelerate_speed : float = 1;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass

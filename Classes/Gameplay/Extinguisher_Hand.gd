dba38cce6e7c5ad412b98ea5ceb21cc2e9ec3ce8
OwO <thien.lee@hcmut.edu.vn>
2026-05-14 03:34:48 +0700

    update audio

--- Classes/Gameplay/Extinguisher_Hand.gd
+++ Classes/Gameplay/Extinguisher_Hand.gd
@@ -2,22 +2,25 @@ class_name ExtinguisherHand
 extends Node3D
 
 @onready var foam_particles: GPUParticles3D = $FoamParticles
+@onready var spray_audio: AudioStreamPlayer3D = $SprayAudio # Thêm biến quản lý âm thanh
 
 @export var damage_per_second: float = 40.0
 
-# Hàm use giờ đây nhận thêm tia ngắm (cam_raycast) từ Player truyền vào
 func use(delta: float, cam_raycast: RayCast3D) -> void:
-	# Bật hiệu ứng phun bọt
+	# Bật bọt và chạy âm thanh (nếu chưa chạy)
 	if not foam_particles.emitting:
 		foam_particles.emitting = true
+		if spray_audio and not spray_audio.playing:
+			spray_audio.play()
 		
-	# Dùng tia ngắm của Camera để dập lửa (đảm bảo luôn bắn trúng tâm màn hình)
 	if cam_raycast and cam_raycast.is_colliding():
 		var target = cam_raycast.get_collider()
 		if target is FireHazard:
 			target.take_damage(damage_per_second * delta)
 
 func stop() -> void:
-	# Tắt hiệu ứng bọt khi nhả chuột
+	# Tắt bọt và dừng âm thanh khi nhả chuột
 	if foam_particles.emitting:
 		foam_particles.emitting = false
+		if spray_audio:
+			spray_audio.stop()
